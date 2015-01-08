becfc184623e0265140de9f174b7b4a8222b55e7
Jeric B. Derama <jericbd@gmail.com>
2015-01-08 03:10:10 -0800

    Directory reorganization pt. 2

--- Problem2_RNA/transcribe.py
+++
@@ -1,9 +0,0 @@
-# First obtain and open the file
-fileName = raw_input("Please input the file name: ")
-if fileName.find('.txt.') == -1:
-    fileName = fileName + '.txt'
-f = open(fileName, 'r')
-
-for line in f:
-    line = line.replace("T","U")
-    print line
